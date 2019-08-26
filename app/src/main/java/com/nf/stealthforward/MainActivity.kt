8db6fca8943333e360a81330ec2f456cc12374f9
Pro Dev <prodev9999@gmail.com>
2019-08-26 17:31:14 +0200

    Minor update

--- app/src/main/java/com/nf/stealthforward/MainActivity.kt
+++ app/src/main/java/com/nf/stealthforward/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.nf.stealthforward
 
+import android.Manifest.permission.SEND_SMS
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity(), SmsListener {
-
-    companion object {
-        lateinit var inst: MainActivity
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +19,7 @@ class MainActivity : AppCompatActivity(), SmsListener {
 
         SmsBroadcastReceiver.smsListener = this
 
-//        fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-
-//        checkForSmsPermission()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        inst = this
+        checkForSmsPermission()
     }
 
     override fun onSmsReceived(sender: String, body: String) {
@@ -38,31 +27,32 @@ class MainActivity : AppCompatActivity(), SmsListener {
         Toast.makeText(this, "$sender : $body", Toast.LENGTH_LONG).show()
     }
 
-//    private fun checkForSmsPermission() {
-//        if (ActivityCompat.checkSelfPermission(
-//                this,
-//                SEND_SMS
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(SEND_SMS),
-//                101
-//            )
-//        }
-//    }
-//
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//
-//        when (requestCode) {
-//            101 -> {
-//                if (!permissions[0].equals(SEND_SMS, true) || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-//                    finish()
-//                }
-//                return
-//            }
-//        }
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//    }
+    private fun checkForSmsPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                SEND_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(SEND_SMS),
+                101
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when (requestCode) {
+            101 -> {
+                if (!permissions[0].equals(SEND_SMS, true) ||
+                    grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "SMS permission denied!", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
 }
