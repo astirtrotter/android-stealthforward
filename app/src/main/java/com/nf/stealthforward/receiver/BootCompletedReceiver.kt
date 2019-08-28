557ccb7fab3618dfa960e5224279042820fe5728
Pro Dev <prodev9999@gmail.com>
2019-08-29 01:18:25 +0800

    Minor bug fix

--- app/src/main/java/com/nf/stealthforward/receiver/BootCompletedReceiver.kt
+++ app/src/main/java/com/nf/stealthforward/receiver/BootCompletedReceiver.kt
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.nf.stealthforward.config.Config
 import com.nf.stealthforward.service.BackgroundService
 
 class BootCompletedReceiver : BroadcastReceiver() {
 
     @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
+        Config.load(context)
         BackgroundService.start(context);
     }
 }
